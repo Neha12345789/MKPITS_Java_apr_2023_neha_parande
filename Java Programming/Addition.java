758827d65a32bd6aec14bf615a39d2ee4328ed0b
neha parande <nehaparande2001@gmail.com>
2023-06-20 10:56:13 +0530

    assignment

---
+++ Java Programming/Addition.java
@@ -0,0 +1,14 @@
+class Substraction
+{
+	public int Add (int num1,int num2)
+	{
+		return num1-num2;
+	}
+	public static void main (String[] args)
+	{
+		Substraction a1 = new Substraction ();
+		int sum = a1 . sub (20,40);
+		System.out.println (sub);
+	}
+}	
+<a href="https://www.w3schools.com/" target="_blank">Visit W3Schools!</a>
---
+++ Java Programming/Additionfloat.java
@@ -0,0 +1,13 @@
+class Additionfloat
+{
+public float Add (float num1, float num2)
+{
+return num1-num2;
+}
+public static void main (string[] args)
+{
+Addition s1 = new Addition();
+float sum=s1.Add(1.2f , 1.5f);
+system.out.println(sum);
+}
+}
