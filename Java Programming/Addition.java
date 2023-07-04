51ac0fc81878c2454c217591486d0b0b474cee32
neha parande <nehaparande2001@gmail.com>
2023-07-04 17:18:01 +0530

    java programs

--- Java Programming/Addition.java
+++ Java Programming/Addition.java
@@ -1,16 +1,1 @@
-class Addition {
-       int add (int a , int b){
-	   return a+b;
-	   }
-	   int add (int a, float b){
-	   return a+b;
-	   }
-	   
-}
 
-public static void main(String[]args){
-        Addition d=new Addition
-	   int sum=d.add(10, 16);
-	   System.out.println(sum);
-          
-}
