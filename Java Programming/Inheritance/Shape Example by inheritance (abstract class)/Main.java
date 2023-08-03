b2c656f806373ce7e6168f1e117d848a6e867ebe
neha parande <nehaparande2001@gmail.com>
2023-08-03 17:19:36 +0530

    Inheritance example with abstract class

---
+++ Java Programming/Inheritance/Shape Example by inheritance (abstract class)/Main.java
@@ -0,0 +1,12 @@
+import java.awt.image.Raster;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Circle circle=new Circle();
+        circle.getCalculateArea();
+        Rectangle rectangle=new Rectangle();
+        rectangle.getCalculateArea();
+    }
+
+}
---
+++ Java Programming/Inheritance/Shape Example by inheritance (abstract class)/Rectangle.java
@@ -0,0 +1,9 @@
+public  class Rectangle extends Shape {
+
+
+    @Override
+    void getCalculateArea() {
+        System.out.println("area of rectangle:-");
+        System.out.println(5*6);
+    }
+}
