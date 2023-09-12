a2834ad6c414e09c36c8690cac13ab483af79568
neha parande <nehaparande2001@gmail.com>
2023-09-12 18:22:58 +0530

    Switch Statement

---
+++ JAVA/SwitchStatement/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        float operator = 3.0f;
+
+        switch (operator){
+
+            case(3.0f);
+                System.out.println(3.0);
+                break;
+
+
+            case(3.00f):
+                System.out.println(3.00);
+                break;
+
+
+            case(3.000f):
+                System.out.println(3.000);
+                break;
+
+
+
+            case(3.0000f):
+                System.out.println(3.0000);
+                break;
+
+            default:
+
+        }
+    }
+}
